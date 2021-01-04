e8781e433bdf8a4bbe0acdc10a41ed4c9b32b37a
Rajeev Dixit <rajeev.dixit@myntra.com>
2021-01-04 14:13:48 +0530

    Added Details page route

--- src/FoodOrdering.js
+++ src/FoodOrdering.js
@@ -5,6 +5,7 @@ import {Route, Switch} from "react-router-dom";
 
 //Imports of different pages in the application
 import Home from './screens/home/Home';
+import Details from "./screens/details/Details";
 
 /**
  * This class represents the whole FoodOrdering Application.
@@ -19,7 +20,8 @@ class FoodOrdering extends Component {
         return (
             <Switch>
                 <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl}/>}/>
-
+                <Route exact path='/restaurant/:restaurantId'
+                       render={(props) => <Details {...props} baseUrl={this.baseUrl}/>}/>
             </Switch>
         )
     }
